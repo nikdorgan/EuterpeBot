abe134b5a3f443f2fc2498c3ce8634aa057dda7e
Nikolas Dorgan <98915416+nikdorgan@users.noreply.github.com>
2022-04-25 15:56:12 +0000

    d

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+const mySecret = process.env['BOT_TOKEN']
+
 require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client(); 
+const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 bot.events = new Discord.Collection();
 
 ['commandHandler', 'eventHandler'].forEach(handler => {
-    require(`./handlers/${handler}`)(bot, Discord);
+  require(`./handlers/${handler}`)(bot, Discord);
 })
 
 ffmpeg_options = {
-    'options': '-vn',
-    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+  'options': '-vn',
+  "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
 }
 
 bot.login(process.env.BOT_TOKEN);
