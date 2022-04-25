2b99afa3fed4508ef6c330667490b202338416cc
Nikolas Dorgan <98915416+nikdorgan@users.noreply.github.com>
2022-04-25 18:29:52 +0000

    d

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-const mySecret = process.env['BOT_TOKEN'];
 
 require('dotenv').config();
 const Discord = require('discord.js');
