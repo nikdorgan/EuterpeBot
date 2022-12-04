f082926e427342584ed3ab0fa3696ee8c6a6e6d7
nikdorgan <nikdorgan@gmail.com>
2022-12-04 17:08:59 -0500

    .

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -4,7 +4,6 @@ module.exports = {
         try {
             serverQueue.songs = [];
             serverQueue.connection.dispatcher.end();
-            voiceChannel.leave();
         } catch (err) {
             voiceChannel.leave();
             console.log(err);
